b755cb0096ca5eba6f81f8771fd5051c1ba80f0f
yulof <dvgnlz@outlook.es>
2020-07-01 17:14:35 +0200

    Sign, percentage and decimal now works and minor changes.

--- src/GUI.java
+++ src/GUI.java
@@ -33,128 +33,67 @@ public class GUI extends JFrame{
         super("Calculadora");
         setContentPane(panel);
 
-        zero.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                numberButtonAction("0");
-            }
+        zero.addActionListener(e -> numberButtonAction("0"));
+
+        one.addActionListener(e -> numberButtonAction("1"));
+
+        two.addActionListener(e -> numberButtonAction("2"));
+
+        three.addActionListener(e -> numberButtonAction("3"));
+
+        four.addActionListener(e -> numberButtonAction("4"));
+
+        five.addActionListener(e -> numberButtonAction("5"));
+
+        six.addActionListener(e -> numberButtonAction("6"));
+
+        seven.addActionListener(e -> numberButtonAction("7"));
+
+        eight.addActionListener(e -> numberButtonAction("8"));
+
+        nine.addActionListener(e -> numberButtonAction("9"));
+
+        C.addActionListener(e -> label.setText("0"));
+
+        add.addActionListener(e -> operationButtonAction('+'));
+
+        subtract.addActionListener(e -> operationButtonAction('-'));
+
+        multiplication.addActionListener(e -> operationButtonAction('*'));
+
+        division.addActionListener(e -> operationButtonAction('/'));
+
+        sign.addActionListener(e -> {
+            if (!label.getText().equals("0"))
+                if (label.getText().charAt(0) != '-')
+                    label.setText("-" + label.getText());
+                else
+                    label.setText(label.getText().replace("-", ""));
         });
 
-        one.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                numberButtonAction("1");
-            }
+        decimal.addActionListener(e -> {
+            if (!label.getText().contains("."))
+                label.setText(label.getText() + '.');
         });
 
-        two.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                numberButtonAction("2");
-            }
-        });
+        percentage.addActionListener(e -> label.setText(String.valueOf(Float.parseFloat(label.getText())/100)));
 
-        three.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                numberButtonAction("3");
-            }
-        });
+        equal.addActionListener(e -> {
+            num2 = Float.parseFloat(label.getText());
 
-        four.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                numberButtonAction("4");
-            }
-        });
+            if (operation == '+')
+                label.setText(result(num1+num2));
 
-        five.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                numberButtonAction("5");
-            }
-        });
+            if (operation == '-')
+                label.setText(result(num1-num2));
 
-        six.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                numberButtonAction("6");
-            }
-        });
+            if (operation == '*')
+                label.setText(result(num1*num2));
 
-        seven.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                numberButtonAction("7");
-            }
-        });
+            if (operation == '/')
+                label.setText(result(num1/num2));
 
-        eight.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                numberButtonAction("8");
-            }
-        });
-
-        nine.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                numberButtonAction("9");
-            }
-        });
-
-        C.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                label.setText("0");
-            }
-        });
-
-        add.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                operationButtonAction('+');
-            }
-        });
-
-        subtract.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                operationButtonAction('-');
-            }
-        });
-
-        multiplication.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                operationButtonAction('*');
-            }
-        });
-
-        division.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                operationButtonAction('/');
-            }
-        });
-
-        equal.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                num2 = Float.parseFloat(label.getText());
-
-                if (operation == '+')
-                    label.setText(String.valueOf(num1+num2));
-
-                if (operation == '-')
-                    label.setText(String.valueOf(num1-num2));
-
-                if (operation == '*')
-                    label.setText(String.valueOf(num1*num2));
-
-                if (operation == '/')
-                    label.setText(String.valueOf(num1/num2));
-            }
+            operation = ' ';
         });
 
     }
@@ -172,20 +111,22 @@ public class GUI extends JFrame{
         label.setText("0");
     }
 
-    public static void view() {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                JFrame frame = new GUI();
-                frame.setSize(340, 480);
-                frame.setVisible(true);
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setResizable(false);
-                frame.setLocationRelativeTo(null);
-            }
-        });
+    private String result(float number){
+        if (number % 1 == 0)
+            return String.valueOf(((int) number));
+        else
+            return String.valueOf(number);
     }
 
-
+    public static void view() {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new GUI();
+            frame.setSize(340, 480);
+            frame.setVisible(true);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setResizable(false);
+            frame.setLocationRelativeTo(null);
+        });
+    }
 
 }
